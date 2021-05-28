3dcae2d14fdebf0d371efdd349bbd54ae7f7ff26
Kittichai <king_atillart_sword3@hotmail.com>
2021-05-28 10:53:52 +0700

    feat: เพิ่ม api สำหรับการส่ง sms ผ่าน pinpoint

---
+++ src/controllers/sms.schema.ts
@@ -0,0 +1,33 @@
+import { FastifySchema } from "fastify";
+
+const responseTemplate: any = {
+  statusCode: { type: 'number' },
+  header: { type: 'object' },
+  body: { type: 'string' },
+}
+
+const smsSchema: FastifySchema = {
+  body: {
+    message: { type: 'string' },
+    phoneNumber: { type: 'string' }
+  },
+  response: {
+    200: {
+      type: 'object',
+      properties: responseTemplate,
+      additionalProperties: false
+    },
+    400: {
+      type: 'object',
+      properties: responseTemplate,
+      additionalProperties: false
+    },
+    403: {
+      type: 'object',
+      properties: responseTemplate,
+      additionalProperties: false
+    }
+  }
+}
+
+export default smsSchema
